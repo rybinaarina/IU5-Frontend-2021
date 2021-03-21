2219aad380610074d0f022f2a0cb640b645ef0bc
Rybina <ribina.arina@mail.ru>
2021-03-21 16:39:56 +0300

    Checking

---
+++ lab3/1.js
@@ -0,0 +1,12 @@
+/**
+ * Напишите функцию capitalize(str),
+ * которая вернет строку str, в которой каждое слово начинается с заглавной буквы.
+ * Примеры:
+ * 'я вижу солнце' -> 'Я Вижу Солнце'
+ * 'я Вижу солнце' -> 'Я Вижу Солнце'
+ */
+function capitalize(str) {
+    return (str.split(/\s+/).map(word => word[0].toUpperCase() + word.substring(1)).join(' '));
+}
+
+module.exports = capitalize;
